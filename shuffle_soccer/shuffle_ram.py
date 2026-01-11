972c045ab869c434de7322bbf512783b1264f93f
Q-bran <bia.ioana.popescu@gmail.com>
2026-01-11 16:57:43 +0100

    more stuff

--- shuffle_soccer/shuffle_ram.py
+++ shuffle_soccer/shuffle_ram.py
@@ -1,25 +1,38 @@
 import numpy as np
 import pandas as pd
-import random
 import csv
+from sklearn.model_selection import train_test_split
 
 dataset_ram = pd.read_csv['C:\Users\bianc\OneDrive\Documents\code\UniBac2\ml-agents\results\soccer\ram_usage_soccer']
-dataset_time = pd.read_csv['C:\Users\bianc\OneDrive\Documents\code\UniBac2\ml-agents\results\soccer\time_per_training']
 
 
-ram = dataset_ram.readlines()
-time = dataset_time.readlines()
+#define x and y 
+x1= dataset_ram.iloc[:,[0,11]].values
+y1= dataset_ram.iloc[[0,10000],:].values   #how many rows ?
 
-#write to new csv 
-with open('shuffle_ram_soccer.csv', 'w',newline='') as csvfile:
-    fieldnames = ['training_run_number','strategy', 'gamePlay', 'batch_size','buffer_size','max_steps','hidden_units','num_layers','CPU_count','CPU_freq_GHz','Total_Phys_RAM_GB','RAM_KB' ]
-    writer = csv.writer(csvfile)
-    writer.writerows(random.shuffle(ram))
+#pre processing
+#!!!
+pd.info()
 
-with open('shuff;e_time_soccer.csv', 'w',newline='') as csvfile:
+x_main1,x_test1,y_main1,y_test1 = train_test_split(x1,y1,test_size=200, stratify=y1) #200 being the size of test set 
+x_train1,x_val1,y_train1,y_val1 = train_test_split(x_main1,y_main1,test_size=200, stratify=y_main1) #200 being the size of the validation set, the rest being the trainign set.
+
+with open('shuffle_time_soccer.csv', 'w',newline='') as csvfile:
     fieldnames = ['strategy', 'gamePlay', 'batch_size','buffer_size','max_steps','hidden_units','num_layers','total_duration' ]
     writer = csv.writer(csvfile)
-    writer.writerows(random.shuffle(time))
+    writer.writerows("\nx_main1:\n")
+    writer.writerows(x_train1.head())
+    writer.writerows(x_train1.shape)
+
+    writer.writerows("\nx_train1:\n")
+    writer.writerows(x_train1.head())
+    writer.writerows(x_train1.shape)
+
+    writer.writerows("\nx_val1:\n")
+    writer.writerows(x_train1.head())
+    writer.writerows(x_train1.shape)
 
 
-#max_steps = 12 mill = end training run 
+
+
+
